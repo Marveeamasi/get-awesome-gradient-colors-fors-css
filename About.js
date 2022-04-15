9c4047f083e6e2b0df18d3895232786d93543cb7
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2022-04-15 14:29:18 +0100

    Create About.js

---
+++ About.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+let x = document.getElementById('listContainer');
+if(x.className === 'listContainer'){
+x.className += ' responsive';
+}
+else{
+x.className === 'listContainer';
+}
+}
